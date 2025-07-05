ed7b26f048b35f69bb4509cd21d1489539d3c171
Md. Zubaer Ahammed <zubaerahammed223@gmail.com>
2025-07-05 22:41:24 +0600

    Adding all semester courses

--- bou_routines_app/models.py
+++ bou_routines_app/models.py
@@ -7,15 +7,15 @@ DAYS = [
 
 class Teacher(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    short_name = models.CharField(max_length=50, blank=True, null=True)
+    name = models.CharField(max_length=100, unique=True)
+    short_name = models.CharField(max_length=50, blank=True, null=True, unique=True)
 
     def __str__(self):
         return self.name
 
 class Semester(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=10, unique=True)
     order = models.IntegerField(default=0, help_text="Display order for semester dropdown")
     semester_full_name = models.CharField(max_length=100, blank=True, null=True)
     term = models.CharField(max_length=50, blank=True, null=True)
@@ -36,8 +36,8 @@ class Semester(models.Model):
 
 class Course(models.Model):
     id = models.AutoField(primary_key=True)
-    code = models.CharField(max_length=20)
-    name = models.CharField(max_length=100, default="")
+    code = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=100, default="", unique=True)
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
 
     def __str__(self):
